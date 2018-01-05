fdfc395e39d9e3b2e923aa71e9a704e2ce928c48
EduardBlack1 <eduard_black1@hotmail.com>
2018-01-05 14:55:26 -0500

    first commit

---
+++ app/src/main/java/com/example/usuario/seccion_02/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.usuario.seccion_02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private List<String> names;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+            //Datos a mostrar
+        names = new ArrayList<String>();
+        names.add("Eduardo");
+        names.add("Luis");
+        names.add("Pedro");
+        names.add("Mario");
+        names.add("Jose");
+
+            //Adaptador, la fonrma de mostrar los datos en la lista
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,names);
+
+        //llenamos el listView con los datos adaptados
+        listView.setAdapter(adapter);
+
+
+        //Mètodo para detectar elemento seleccionado de la vista
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+          @Override
+          public void onItemClick(AdapterView<?> adapterView, View view,int position,long id){
+
+              Toast.makeText(MainActivity.this,"Clicked: "+ names.get(position), Toast.LENGTH_LONG).show();
+
+          }
+
+        });
+
+        //Enlazamos con nuestro adaptador personalizado
+        MyAdapter myAdapter = new MyAdapter(this, R.layout.list_item, names);
+        listView.setAdapter(myAdapter);
+    }
+}
+
---
+++ app/src/main/java/com/example/usuario/seccion_02/MyAdapter.java
@@ -0,0 +1,62 @@
+package com.example.usuario.seccion_02;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MyAdapter extends BaseAdapter {
+
+    private Context context;
+    private int layout;
+    private List<String> names;
+
+    public MyAdapter(Context context, int layout, List<String> names) {
+        this.context = context;
+        this.layout = layout;
+        this.names = names;
+    }
+
+    @Override
+    public int getCount(){
+        return this.names.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return this.names.get(position);
+    }
+
+    @Override
+    public long getItemId(int id) {
+        return id;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+
+        //Copiamos la vista
+        View v = convertView;
+
+        //Se infla el layout y de copia el formato de lista a la nueva
+        LayoutInflater layoutInflater = LayoutInflater.from(this.context);
+        v = layoutInflater.inflate(R.layout.list_item,null);
+
+        //Traemos el nombre actual dependiendo de la posiciòn
+        String currentName = names.get(position);
+        //currentName = (String) getItem(position);
+
+
+        //Referenciamos el elemnto a modificar y es llenado
+        TextView textView = (TextView) v.findViewById(R.id.textView);
+        textView.setText(currentName);
+
+        //se devuelve la lista llena y modificada
+        return v;
+
+    }
+}
